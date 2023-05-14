a622661f8eea05549ab235d0f7b3bb933a77489c
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-05-14 20:53:55 +0000

    Define a function create_battleship_board that takes in two parameters, row and cols

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Function to create the game board
+def create_battleship_board(rows, cols):
+    board = []
+    for i in range(rows):
+        board.append(["O"] * cols)
+    return board
