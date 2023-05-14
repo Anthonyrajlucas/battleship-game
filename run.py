6e66cbcbde5900d086b2cd6781a693dc2625196c
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-05-14 21:02:15 +0000

    Define function place_ship board, num_ships that takes in two parameters, board representing the game board and num_ships representing the number of battleships to be placed on the board

--- run.py
+++ run.py
@@ -4,3 +4,13 @@ def create_battleship_board(rows, cols):
     for i in range(rows):
         board.append(["O"] * cols)
     return board
+
+# Function to randomly place the battleships on the board
+def place_ships_on_the_board(board, num_ships):
+    ships_placed = 0
+    while ships_placed < num_ships:
+        row = random.randint(0, len(board) - 1)
+        col = random.randint(0, len(board[0]) - 1)
+        if board[row][col] != "S":
+            board[row][col] = "S"
+            ships_placed += 1    
