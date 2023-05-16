ef1cb6ff8dc158e14f106b223da1761472129de9
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-05-16 20:06:26 +0000

    Added number of ships and width of the ships are handled via input

--- run.py
+++ run.py
@@ -10,26 +10,26 @@ def create_battleship_board(rows, cols):
 # Function to print the game board
 def print_battleship_board(board):
     for row in board:
-        print(" ".join(row))    
-
+        print(" ".join(row))
 
 # Function to randomly place the battleships on the board
-def place_ships_on_the_board(board, num_ships):
+def place_ships_on_the_board(board, num_ships, ship_widths):
     ships_placed = 0
     while ships_placed < num_ships:
         row = random.randint(0, len(board) - 1)
-        col = random.randint(0, len(board[0]) - 1)
-        if board[row][col] != "S":
-            board[row][col] = "S"
-            ships_placed += 1    
-
+        col = random.randint(0, len(board[0]) - ship_widths[ships_placed])
+        for i in range(ship_widths[ships_placed]):
+            if board[row][col + i] == "S":
+                break
+        else:
+            for i in range(ship_widths[ships_placed]):
+                board[row][col + i] = "S"
+            ships_placed += 1
 
 # Function to check if a guess is on the board
 def is_on_board(guess_row, guess_col, rows, cols):
     return guess_row >= 0 and guess_row < rows and \
            guess_col >= 0 and guess_col < cols
-
-
 
 # Function to get the user's guess
 def get_guess_from_player(rows, cols):
@@ -41,15 +41,19 @@ def get_guess_from_player(rows, cols):
         else:
             print("Oops, that's not even in the ocean.")
 
-# Set the grid size
+# Set the grid size and ship widths
 rows = int(input("Enter the number of rows: "))
-cols = int(input("Enter the number of columns: "))  
+cols = int(input("Enter the number of columns: "))
+num_ships = int(input("Enter the number of ships: "))
+
+ship_widths = []
+for i in range(num_ships):
+    width = int(input("Enter the width for ship {}: ".format(i + 1)))
+    ship_widths.append(width)
 
 # Create the game board and place the battleships
 board = create_battleship_board(rows, cols)
-num_ships = min(rows, cols) // 2
-place_ships_on_the_board(board, num_ships)
-
+place_ships_on_the_board(board, num_ships, ship_widths)
 
 # Play the game
 turns = num_ships * 2
